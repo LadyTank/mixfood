2d7978e76d7c151345144f8ebed05b23d1f22391
baroudeColombbus <69351692+baroudeColombbus@users.noreply.github.com>
2021-05-30 15:25:58 +0200

    update du des fichier sushi.php puis pizza.php

--- sushi.php
+++ sushi.php
@@ -1,195 +1,86 @@
 <?php
+// author: baroude ntsiba
+// contributor : Audrey
+// contributor : Alexandre 
 include 'inc/init.php';
 include_once 'inc/haut.php';
+
+$sql =  $pdoSITE->prepare("SELECT p.produit_prix, p.id_produit, p.produit_image, p.nom_produit, p.produit_ingredients
+FROM produit_categorie pc , produit p
+WHERE pc.id_categorie=p.id_categorie  AND pc.nom_categorie='sushi' and p.produit_disponible='oui'");
+
+//execute the sql statement as an object NOT an array
+
+$sql->execute();
+
+// fetch all rows into array, by default PDO::FETCH_BOTH is used
+$result =   $sql->fetchAll();
+//var_dump($result);
+
+// count the number of admin in the database
+$nbr_food =      $sql->rowCount();
+
 ?>
-
 <!-- ==================================================== -->
-<!-- ==================== Card Sushi ==================== -->
+<!-- ==================== Card Pizza ==================== -->
 <!-- ==================================================== -->
-
-<h1 class="titreChoix text-white text-center mt-5">Nos Sushis</h1>
+<h1 class="titreChoix text-white text-center mt-5">Nos Pizzas</h1>
 <div class="container bg-dark py-2 pt-5 pb-4 my-3 img-curvy img-thumbnail img-responsive" style="background-color:#28a745; border-color:#28a745;">
+    <!-- <div class="row text-center border border-danger">
+        <div class="col-12 col-md-6 col-lg-4 mb-3 border border-primary"> -->
 
-    <div class=" row text-center">
-        <div class="col-12 col-md-6 col-lg-4 mb-3">
-            <img src="img/sushi1-300.jpg" alt="pizza" class="img-curvy img-thumbnail" style="background-color:#28a745; border-color:#28a745;">
-            <!-- accordeon -->
-            <div class="accordion accordion-flush my-2 mx-auto" id="accordionS1" style="width:50%">
-                <div class="accordion-item bg-success">
-                    <h2 class="accordion-header" id="flush-head-accordionS1">
-                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionS1" aria-expanded="false" aria-controls="flush-accordionS1" style="background-color: #0a5846ab; color: #fff">
-                            Avocato 5.99€
+    <?php
+    if ($nbr_food > 0) {
+        echo ' <div class="row text-center ">';
 
-                            <?php
+        // there are record in the database
 
-                            ?>
+        foreach ($result as $row) {
+            $id_produit = $row['id_produit'];
+            $nom_produit = $row['nom_produit'];
+            $produit_image  = $row['produit_image'];
+            $produit_prix = $row['produit_prix'];
+            $produit_ingredients = $row['produit_ingredients'];
 
-                        </button>
-                    </h2>
-                    <div id="flush-accordionS1" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionS1" data-bs-parent="#accordionS1">
-                        <div class="accordion-body">
-                            <p> Riz, avocat, feuille d'algue
+            echo ' <div class="col-12 col-md-6 col-lg-4 mb-3">';
 
-                                <?php
 
-                                ?>
+            if ($produit_image == '') {
+                // Display an error message 
+                echo 'image nexiste pas';
+            } else {
 
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div> <!-- Fin accordeon -->
-        </div><!-- Fin de col card -->
 
-        <div class="col-12 col-md-6 col-lg-4 mb-3">
-            <img src="img/sushi2-300.jpg" alt="pizza" class="img-curvy img-thumbnail" style="background-color:#28a745; border-color:#28a745;">
-            <!-- accordeon -->
-            <div class="accordion accordion-flush my-2 mx-auto" id="accordionS2" style="width:50%">
-                <div class="accordion-item bg-success">
-                    <h2 class="accordion-header" id="flush-head-accordionS2">
-                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionS2" aria-expanded="false" aria-controls="flush-accordionS2" style="background-color: #0a5846ab; color: #fff">
-                            Sushi Maki Nigiri 5.99€
+                // Display the image
+                // ici tu peux rendre l image clickable
+                echo '<a href="' . SITEURL . '/pizza.php ">';
+                echo '<img src="' . SITEURL . 'img/produit/' . $produit_image . '" class="img-curvy img-thumbnail img-responsive" style="background-color:#28a745; border-color:#28a745;"></a>';
+                echo '</a>';
+                echo '<div class="accordion accordion-flush my-2 mx-auto" id="accordionP6" style="width:50%">';
+                echo '<div class="accordion-item bg-success">';
+                '<h2 class="accordion-header" id="flush-head-accordionP6">';
 
-                            <?php
+                echo ' <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionP6" aria-expanded="false" aria-controls="flush-accordionP6" style="background-color: #0a5846ab; color: #fff">' . $produit_prix . ' €' .
+                    '</button>';
+                echo ' </h2>';
 
-                            ?>
+                echo   ' <div id="flush-accordionP6" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionP6" data-bs-parent="#accordionP6">';
+                echo '<div class="accordion-body">';
+                echo  ' <p>'  . $produit_ingredients . '</p>';
+                echo            '</div>';
+                echo    ' </div>';
+                echo ' </div>';
+                echo '</div>';
+            }
 
-                        </button>
-                    </h2>
-                    <div id="flush-accordionS2" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionS2" data-bs-parent="#accordionS2">
-                        <div class="accordion-body">
-                            <p> Riz, avocat, feuille d'algue
+            echo '</div>';
+        }
 
-                                <?php
+        echo '</div>';
+    }
+    ?>
 
-                                ?>
 
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div> <!-- Fin accordeon -->
-        </div><!-- Fin de col card -->
-
-        <div class="col-12 col-md-6 col-lg-4 mb-3">
-            <img src="img/sushi3-300.jpg" alt="pizza" class="img-curvy img-thumbnail" style="background-color:#28a745; border-color:#28a745;">
-            <!-- accordeon -->
-            <div class="accordion accordion-flush my-2 mx-auto" id="accordionS3" style="width:50%">
-                <div class="accordion-item bg-success">
-                    <h2 class="accordion-header" id="flush-head-accordionS3">
-                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionS3" aria-expanded="false" aria-controls="flush-accordionS3" style="background-color: #0a5846ab; color: #fff">
-                            Te Maki 5.99€
-
-                            <?php
-
-                            ?>
-
-                        </button>
-                    </h2>
-                    <div id="flush-accordionS3" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionS3" data-bs-parent="#accordionS3">
-                        <div class="accordion-body">
-                            <p> Riz, avocat, feuille d'algue
-
-                                <?php
-
-                                ?>
-
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div> <!-- Fin accordeon -->
-        </div><!-- Fin de col card -->
-
-        <div class="col-12 col-md-6 col-lg-4 mb-3">
-            <img src="img/sushi4-300.jpg" alt="pizza" class="img-curvy img-thumbnail" style="background-color:#28a745; border-color:#28a745;">
-            <!-- accordeon -->
-            <div class="accordion accordion-flush my-2 mx-auto" id="accordionS4" style="width:50%">
-                <div class="accordion-item bg-success">
-                    <h2 class="accordion-header" id="flush-head-accordionS4">
-                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionS4" aria-expanded="false" aria-controls="flush-accordionS4" style="background-color: #0a5846ab; color: #fff">
-                            Inaru Zushi 5.99€
-
-                            <?php
-
-                            ?>
-
-                        </button>
-                    </h2>
-                    <div id="flush-accordionS4" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionS4" data-bs-parent="#accordionS4">
-                        <div class="accordion-body">
-                            <p> Riz, avocat, feuille d'algue
-
-                                <?php
-
-                                ?>
-
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div> <!-- Fin accordeon -->
-        </div><!-- Fin de col card -->
-
-        <div class="col-12 col-md-6 col-lg-4 mb-3">
-            <img src="img/sushi5-300.jpg" alt="pizza" class="img-curvy img-thumbnail" style="background-color:#28a745; border-color:#28a745;">
-            <!-- accordeon -->
-            <div class="accordion accordion-flush my-2 mx-auto" id="accordionS5" style="width:50%">
-                <div class="accordion-item bg-success">
-                    <h2 class="accordion-header" id="flush-head-accordionS5">
-                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionS5" aria-expanded="false" aria-controls="flush-accordionS5" style="background-color: #0a5846ab; color: #fff">
-                            California Rolls 5.99€
-
-                            <?php
-
-                            ?>
-
-                        </button>
-                    </h2>
-                    <div id="flush-accordionS5" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionS5" data-bs-parent="#accordionS5">
-                        <div class="accordion-body">
-                            <p> Riz, avocat, feuille d'algue
-
-                                <?php
-
-                                ?>
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div> <!-- Fin accordeon -->
-        </div><!-- Fin de col card -->
-
-        <div class="col-12 col-md-6 col-lg-4 mb-3">
-            <img src="img/sushi6-300.jpg" alt="pizza" class="img-curvy img-thumbnail" style="background-color:#28a745; border-color:#28a745;">
-            <!-- accordeon -->
-            <div class="accordion accordion-flush my-2 mx-auto" id="accordionS6" style="width:50%">
-                <div class="accordion-item bg-success">
-                    <h2 class="accordion-header" id="flush-head-accordionS6">
-                        <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionS6" aria-expanded="false" aria-controls="flush-accordionS6" style="background-color: #0a5846ab; color: #fff">
-                            Oshi Sushi 5.99€
-
-                            <?php
-
-                            ?>
-
-                        </button>
-                    </h2>
-                    <div id="flush-accordionS6" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionS6" data-bs-parent="#accordionS6">
-                        <div class="accordion-body">
-                            <p> Riz, avocat, feuille d'algue
-
-                                <?php
-
-                                ?>
-
-                            </p>
-                        </div>
-                    </div>
-                </div>
-            </div> <!-- Fin accordeon -->
-        </div><!-- Fin de col card -->
-    </div>
 </div>
 <?php
 include_once 'inc/bas.php';
