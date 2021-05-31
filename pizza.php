4db2f3935d2e2c887c0586388f0b1c9338d91be3
LadyTank <69342492+LadyTank@users.noreply.github.com>
2021-05-31 14:48:53 +0200

    id sushi pizza

--- pizza.php
+++ pizza.php
@@ -1,7 +1,5 @@
 <?php
-// author: baroude ntsiba
-// contributor : Audrey
-// contributor : Alexandre 
+
 include 'inc/init.php';
 include_once 'inc/haut.php';
 
@@ -50,7 +48,7 @@ $nbr_food =  $sql->rowCount();
             // si l'image du produit est vide ?
             if ($produit_image == '') {
                 // on affiche ce message 
-                echo 'image nexiste pas';
+                echo '<p>l\'image n\'existe pas</p>';
             } else { //sinon
 
 
@@ -59,16 +57,16 @@ $nbr_food =  $sql->rowCount();
                 // 
                 echo '<img src="' . SITEURL . 'img/produit/' . $produit_image . '" class="img-curvy img-thumbnail img-responsive" style="background-color:#28a745; border-color:#28a745;">';
                 // en bas nous avont le code html de l'accordion plus le bouton
-                echo '<div class="accordion accordion-flush my-2 mx-auto" id="accordionP6" style="width:50%">';
+                echo '<div class="accordion accordion-flush my-2 mx-auto" id="accordionP" style="width:50%">';
                 echo '<div class="accordion-item bg-success">';
-                '<h2 class="accordion-header" id="flush-head-accordionP6">'; 
-                echo ' <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionP6" aria-expanded="false" aria-controls="flush-accordionP6" style="background-color: #0a5846ab; color: #fff">' . $produit_prix . ' €' .
+                '<h2 class="accordion-header" id="flush-head-accordionP">';
+                echo ' <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionP" aria-expanded="false" aria-controls="flush-accordionP" style="background-color: #0a5846ab; color: #fff">' . $produit_prix . ' €' .
                     '</button>';
                 echo ' </h2>';
 
-                echo   ' <div id="flush-accordionP6" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionP6" data-bs-parent="#accordionP6">';
+                echo   ' <div id="flush-accordionP" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionP" data-bs-parent="#accordionP">';
                 echo '<div class="accordion-body">';
-                echo  ' <p>'  . $produit_ingredients . '</p>';// on affiche les incgrédientd
+                echo  ' <p>'  . $produit_ingredients . '</p>'; // on affiche les incgrédientd
                 echo            '</div>';
                 echo    ' </div>';
                 echo ' </div>';
@@ -76,7 +74,7 @@ $nbr_food =  $sql->rowCount();
             }
 
             echo '</div>';
-        }// fin foreach
+        } // fin foreach
 
         echo '</div>';
     }
--- sushi.php
+++ sushi.php
@@ -1,7 +1,5 @@
 <?php
-// author: baroude ntsiba
-// contributor : Audrey
-// contributor : Alexandre 
+
 include 'inc/init.php';
 include_once 'inc/haut.php';
 
@@ -9,11 +7,11 @@ $sql = $pdoSITE->prepare("SELECT p.produit_prix, p.id_produit, p.produit_image, 
 FROM produit_categorie pc , produit p
 WHERE pc.id_categorie=p.id_categorie  AND pc.nom_categorie='sushi' and p.produit_disponible='oui'");
 
-//execute the sql statement as an object NOT an array
+//execute la requête
 
 $sql->execute();
 
-// fetch all rows into array, by default PDO::FETCH_BOTH is used
+// on récupère toutes les données par defaut fetchAll utilise PDO::FETCH_BOTH 
 $result = $sql->fetchAll();
 //var_dump($result);
 
@@ -22,14 +20,14 @@ $nbr_food =  $sql->rowCount();
 
 ?>
 <!-- ==================================================== -->
-<!-- ==================== Card Pizza ==================== -->
+<!-- ==================== Card sushi ==================== -->
 <!-- ==================================================== -->
 <h1 class="titreChoix text-white text-center mt-5">Nos sushis</h1>
 <div class="container bg-dark py-2 pt-5 pb-4 my-3 img-curvy img-thumbnail img-responsive" style="background-color:#28a745; border-color:#28a745;">
     <!-- <div class="row text-center border border-danger">
         <div class="col-12 col-md-6 col-lg-4 mb-3 border border-primary"> -->
 
-        <?php
+    <?php
     // si il y a des données
     if ($nbr_food > 0) {
         echo '<div class="row text-center">';
@@ -51,7 +49,7 @@ $nbr_food =  $sql->rowCount();
             // si l'image du produit est vide ?
             if ($produit_image == '') {
                 // on affiche ce message 
-                echo 'image nexiste pas';
+                echo '<p>l\'image n\'existe pas</p>';
             } else { //sinon
 
 
@@ -60,16 +58,16 @@ $nbr_food =  $sql->rowCount();
                 // 
                 echo '<img src="' . SITEURL . 'img/produit/' . $produit_image . '" class="img-curvy img-thumbnail img-responsive" style="background-color:#28a745; border-color:#28a745;">';
                 // en bas nous avont le code html de l'accordion plus le bouton
-                echo '<div class="accordion accordion-flush my-2 mx-auto" id="accordionP6" style="width:50%">';
+                echo '<div class="accordion accordion-flush my-2 mx-auto" id="accordionS" style="width:50%">';
                 echo '<div class="accordion-item bg-success">';
-                '<h2 class="accordion-header" id="flush-head-accordionP6">'; 
-                echo ' <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionP6" aria-expanded="false" aria-controls="flush-accordionP6" style="background-color: #0a5846ab; color: #fff">' . $produit_prix . ' €' .
+                '<h2 class="accordion-header" id="flush-head-accordionS">';
+                echo ' <button class="accordion-button collapsed btn" type="button" data-bs-toggle="collapse" data-bs-target="#flush-accordionS" aria-expanded="false" aria-controls="flush-accordionS" style="background-color: #0a5846ab; color: #fff">' . $produit_prix . ' €' .
                     '</button>';
                 echo ' </h2>';
 
-                echo   ' <div id="flush-accordionP6" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionP6" data-bs-parent="#accordionP6">';
+                echo   ' <div id="flush-accordionS" class="accordion-collapse collapse bg-success" aria-labelledby="flush-head-accordionS" data-bs-parent="#accordionS">';
                 echo '<div class="accordion-body">';
-                echo  ' <p>'  . $produit_ingredients . '</p>';// on affiche les incgrédientd
+                echo  ' <p>'  . $produit_ingredients . '</p>'; // on affiche les incgrédientd
                 echo            '</div>';
                 echo    ' </div>';
                 echo ' </div>';
@@ -77,7 +75,7 @@ $nbr_food =  $sql->rowCount();
             }
 
             echo '</div>';
-        }// fin foreach
+        } // fin foreach
 
         echo '</div>';
     }
