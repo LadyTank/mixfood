c11bac906e8b0bcf3bbb9c24d759336074010f7f
LadyTank <69342492+LadyTank@users.noreply.github.com>
2021-05-31 17:04:17 +0200

    bas

---
+++ newsletter.view.php
@@ -0,0 +1,5 @@
+<form method="POST">
+    <label>Adresse e-mail</label><br />
+    <input type="email" name="newsletter" /><br /><br />
+    <input type="submit" name="newsletterform" value="Envoyer" />
+</form>
